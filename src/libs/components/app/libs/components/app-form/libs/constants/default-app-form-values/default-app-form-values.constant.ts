4ed789b3f8ff826b4ef78a22b7027363798763b7
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-01-04 13:28:57 +0200

    le-04: + defaultAppFormValues constant

---
+++ src/libs/components/app/libs/components/app-form/libs/constants/default-app-form-values/default-app-form-values.constant.ts
@@ -0,0 +1,5 @@
+import { AppFormFields } from "../../types/app-form-fields.type.js";
+
+const DEFAULT_APP_FORM_VALUES: AppFormFields = { name: "", lastName: "" };
+
+export { DEFAULT_APP_FORM_VALUES };
