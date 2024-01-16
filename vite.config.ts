7cebe4195b7abcb94676e16e85d67b3420b6f152
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-01-16 12:49:14 +0200

    le-06: * replace icons source to heroicons library
    Replace icons implementation because it says so in the description of
    the task

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
   },
