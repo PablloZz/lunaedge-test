40ac9eda55b405bd7d747e4cb892bf6efe4fa4c0
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-01-19 16:00:48 +0200

    le-06: - unnecessary folder for a constant

---
+++ src/libs/components/app/libs/components/app-form/libs/constants/default-app-form-values.constant.ts
@@ -0,0 +1,8 @@
+import { AppFormFields } from "../types/app-form-fields.type.js";
+
+const DEFAULT_APP_FORM_VALUES: AppFormFields = {
+  name: "",
+  lastName: "",
+};
+
+export { DEFAULT_APP_FORM_VALUES };
--- src/libs/components/app/libs/components/app-form/libs/constants/default-app-form-values/default-app-form-values.constant.ts
+++
@@ -1,5 +0,0 @@
-import { AppFormFields } from "../../types/app-form-fields.type.js";
-
-const DEFAULT_APP_FORM_VALUES: AppFormFields = { name: "", lastName: "" };
-
-export { DEFAULT_APP_FORM_VALUES };
