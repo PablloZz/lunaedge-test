b37c1100bed639447439b43c6830b0857482b14d
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-01-19 16:01:36 +0200

    le-06: * app form type and default values constant
    Add a pokemon property

--- src/libs/components/app/libs/components/app-form/libs/constants/default-app-form-values.constant.ts
+++ src/libs/components/app/libs/components/app-form/libs/constants/default-app-form-values.constant.ts
@@ -3,6 +3,7 @@ import { AppFormFields } from "../types/app-form-fields.type.js";
 const DEFAULT_APP_FORM_VALUES: AppFormFields = {
   name: "",
   lastName: "",
+  pokemon: [],
 };
 
 export { DEFAULT_APP_FORM_VALUES };
